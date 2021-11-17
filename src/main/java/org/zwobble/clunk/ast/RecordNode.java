fa279d72c7f9024980cfde87f8e94c5839d4e8a9
Michael Williamson <mike@zwobble.org>
2021-11-17 20:06:03 +0000

    Add builder for RecordNode

--- src/main/java/org/zwobble/clunk/ast/RecordNode.java
+++ src/main/java/org/zwobble/clunk/ast/RecordNode.java
@@ -1,8 +1,25 @@
 package org.zwobble.clunk.ast;
 
+import org.zwobble.clunk.sources.NullSource;
 import org.zwobble.clunk.sources.Source;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record RecordNode(String name, List<RecordFieldNode> fields, Source source) implements NamespaceStatementNode {
+    public static Builder builder(String name) {
+        return new Builder(name, List.of(), NullSource.INSTANCE);
+    }
+
+    public static record Builder(String name, List<RecordFieldNode> fields, Source source) {
+        public RecordNode build() {
+            return new RecordNode(name, fields, source);
+        }
+
+        public Builder addField(RecordFieldNode field) {
+            var fields = new ArrayList<>(this.fields);
+            fields.add(field);
+            return new Builder(name, fields, source);
+        }
+    }
 }
--- src/test/java/org/zwobble/clunk/backends/java/JavaCodeGeneratorTests.java
+++ src/test/java/org/zwobble/clunk/backends/java/JavaCodeGeneratorTests.java
@@ -7,22 +7,16 @@ import org.zwobble.clunk.ast.StaticReferenceNode;
 import org.zwobble.clunk.backends.java.ast.JavaSerialiser;
 import org.zwobble.clunk.sources.NullSource;
 
-import java.util.List;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class JavaCodeGeneratorTests {
     @Test
     public void recordIsCompiledToJavaRecord() {
-        var node = new RecordNode(
-            "Example",
-            List.of(
-                new RecordFieldNode("first", new StaticReferenceNode("String", NullSource.INSTANCE), NullSource.INSTANCE),
-                new RecordFieldNode("second", new StaticReferenceNode("Int", NullSource.INSTANCE), NullSource.INSTANCE)
-            ),
-            NullSource.INSTANCE
-        );
+        var node = RecordNode.builder("Example")
+            .addField(new RecordFieldNode("first", new StaticReferenceNode("String", NullSource.INSTANCE), NullSource.INSTANCE))
+            .addField(new RecordFieldNode("second", new StaticReferenceNode("Int", NullSource.INSTANCE), NullSource.INSTANCE))
+            .build();
 
         var result = JavaCodeGenerator.compileRecord(node);
         var stringBuilder = new StringBuilder();

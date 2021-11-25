816d744fac665ded88817d583a309a8f5a402f93
Michael Williamson <mike@zwobble.org>
2021-11-25 18:00:53 +0000

    Introduce PythonExpressionNode

---
+++ src/main/java/org/zwobble/clunk/backends/python/ast/PythonExpressionNode.java
@@ -0,0 +1,9 @@
+package org.zwobble.clunk.backends.python.ast;
+
+public interface PythonExpressionNode extends PythonNode {
+    <T> T accept(Visitor<T> visitor);
+
+    interface Visitor<T> {
+        T visit(PythonReferenceNode node);
+    }
+}
--- src/main/java/org/zwobble/clunk/backends/python/ast/PythonReferenceNode.java
+++ src/main/java/org/zwobble/clunk/backends/python/ast/PythonReferenceNode.java
@@ -1,5 +1,8 @@
 package org.zwobble.clunk.backends.python.ast;
 
-public record PythonReferenceNode(String name) implements PythonNode {
-
+public record PythonReferenceNode(String name) implements PythonExpressionNode {
+    @Override
+    public <T> T accept(Visitor<T> visitor) {
+        return visitor.visit(this);
+    }
 }

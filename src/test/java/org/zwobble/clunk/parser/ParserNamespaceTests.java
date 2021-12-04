ffbd7ee09d3ce889ff317f0555a31115388b1124
Michael Williamson <mike@zwobble.org>
2021-12-04 16:19:37 +0000

    Remove redundant tests

--- src/test/java/org/zwobble/clunk/parser/ParserNamespaceTests.java
+++ src/test/java/org/zwobble/clunk/parser/ParserNamespaceTests.java
@@ -24,44 +24,6 @@ public class ParserNamespaceTests {
         )));
     }
 
-    @Test
-    public void canParseSingleField() {
-        var source = "record User(name: String)";
-
-        var tokens = Tokeniser.tokenise(source);
-        var node = parser().parseNamespaceStatement(tokens);
-
-        assertThat(node, isUntypedRecordNode(has("fields", contains(
-            allOf(has("name", equalTo("name")), has("type", isUntypedStaticReferenceNode("String")))
-        ))));
-    }
-
-    @Test
-    public void canParseMultipleFields() {
-        var source = "record User(name: String, emailAddress: String)";
-
-        var tokens = Tokeniser.tokenise(source);
-        var node = parser().parseNamespaceStatement(tokens);
-
-        assertThat(node, isUntypedRecordNode(has("fields", contains(
-            has("name", equalTo("name")),
-            has("name", equalTo("emailAddress"))
-        ))));
-    }
-
-    @Test
-    public void fieldsCanHaveTrailingComma() {
-        var source = "record User(name: String, emailAddress: String,)";
-
-        var tokens = Tokeniser.tokenise(source);
-        var node = parser().parseNamespaceStatement(tokens);
-
-        assertThat(node, isUntypedRecordNode(has("fields", contains(
-            has("name", equalTo("name")),
-            has("name", equalTo("emailAddress"))
-        ))));
-    }
-
     private Parser parser() {
         return new Parser("<filename>", "<contents>");
     }

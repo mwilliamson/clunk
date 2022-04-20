8c678f521149287d24e3ae51e0e9ed0c266c7b21
Michael Williamson <mike@zwobble.org>
2022-04-20 19:20:43 +0100

    Check that Java name for test is valid

--- src/main/java/org/zwobble/clunk/backends/java/codegenerator/JavaTestNames.java
+++ src/main/java/org/zwobble/clunk/backends/java/codegenerator/JavaTestNames.java
@@ -1,12 +1,22 @@
 package org.zwobble.clunk.backends.java.codegenerator;
 
+import org.zwobble.clunk.errors.InternalCompilerError;
+
 import java.util.Locale;
 import java.util.regex.Pattern;
 
 public class JavaTestNames {
+    private static final Pattern VALID_IDENTIFIER = Pattern.compile("^[a-z][A-Za-z0-9]*$");
+
     public static String generateName(String name) {
-        return Pattern.compile(" ([a-z])")
+        var identifier = Pattern.compile(" ([a-z])")
             .matcher(name)
             .replaceAll(result -> result.group(1).toUpperCase(Locale.ROOT));
+
+        if (!VALID_IDENTIFIER.matcher(identifier).matches()) {
+            throw new InternalCompilerError("Could not convert test name to Java identifier: " + name);
+        }
+
+        return identifier;
     }
 }
---
+++ src/test/java/org/zwobble/clunk/backends/java/codegenerator/JavaTestNamesTests.java
@@ -0,0 +1,27 @@
+package org.zwobble.clunk.backends.java.codegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.zwobble.clunk.errors.InternalCompilerError;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class JavaTestNamesTests {
+    @Test
+    public void testNameIsConvertedToCamelCase() {
+        var result = JavaTestNames.generateName("can assign bool");
+
+        assertThat(result, equalTo("canAssignBool"));
+    }
+
+    @Test
+    public void whenNameIsNotValidIdentifierThenErrorIsThrown() {
+        var result = assertThrows(
+            InternalCompilerError.class,
+            () -> JavaTestNames.generateName("☃")
+        );
+
+        assertThat(result.getMessage(), equalTo("Could not convert test name to Java identifier: ☃"));
+    }
+}

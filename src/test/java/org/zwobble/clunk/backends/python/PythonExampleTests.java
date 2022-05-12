a34aeaba8aac030e36f54e239119d8b241848ef5
Michael Williamson <mike@zwobble.org>
2022-05-12 09:23:07 +0100

    Extract deleteRecursive

--- src/test/java/org/zwobble/clunk/backends/python/PythonExampleTests.java
+++ src/test/java/org/zwobble/clunk/backends/python/PythonExampleTests.java
@@ -18,6 +18,7 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import static org.zwobble.clunk.testing.ProjectRoot.findRoot;
+import static org.zwobble.clunk.util.DeleteRecursive.deleteRecursive;
 
 @ExtendWith(SnapshotResolver.class)
 public class PythonExampleTests {
@@ -82,27 +83,4 @@ public class PythonExampleTests {
             deleteRecursive(outputRoot);
         }
     }
-
-    private static void deleteRecursive(Path path) throws IOException {
-        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
-            @Override
-            public FileVisitResult visitFile(
-                Path file,
-                BasicFileAttributes attrs
-            ) throws IOException {
-                Files.delete(file);
-                return FileVisitResult.CONTINUE;
-            }
-
-            @Override
-            public FileVisitResult postVisitDirectory(
-                Path dir,
-                IOException exc
-            ) throws IOException {
-                Files.delete(dir);
-                return FileVisitResult.CONTINUE;
-            }
-        });
-    }
-
 }

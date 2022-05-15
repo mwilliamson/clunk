03ed7d351d1c284992cc2016998b13af88202aca
Michael Williamson <mike@zwobble.org>
2022-05-15 22:03:12 +0100

    Support example tests for TypeScript

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 .PHONY: setup
 
-setup: testing/python/_virtualenv
+setup: testing/python/_virtualenv testing/typescript
 
 testing/python/_virtualenv:
 	python3 -m venv testing/python/_virtualenv
@@ -8,3 +8,6 @@ testing/python/_virtualenv:
 	testing/python/_virtualenv/bin/pip install --upgrade setuptools
 	testing/python/_virtualenv/bin/pip install --upgrade wheel
 	testing/python/_virtualenv/bin/pip install precisely==0.1.9 pytest==7.1.2
+
+testing/typescript:
+
--- src/test/java/org/zwobble/clunk/backends/typescript/TypeScriptTestRunner.java
+++ src/test/java/org/zwobble/clunk/backends/typescript/TypeScriptTestRunner.java
@@ -2,12 +2,55 @@ package org.zwobble.clunk.backends.typescript;
 
 import org.zwobble.clunk.backends.TargetTestRunner;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.zwobble.clunk.testing.ProjectRoot.findRoot;
+import static org.zwobble.clunk.util.DeleteRecursive.deleteRecursive;
 
 public class TypeScriptTestRunner implements TargetTestRunner {
     @Override
     public String runTests(Path outputRoot) throws IOException, InterruptedException {
-        return "";
+        var typeScriptRoot = findRoot().resolve("testing/typescript");
+        var nodeModules = typeScriptRoot.resolve("node_modules");
+
+        var srcPath = typeScriptRoot.resolve("src");
+        if (srcPath.toFile().exists()) {
+            deleteRecursive(srcPath);
+        }
+        var copyResult = new ProcessBuilder("cp", "-r", outputRoot.toString(), srcPath.toString())
+            .start()
+            .waitFor();
+        assertThat(copyResult, equalTo(0));
+
+        var processBuilder = new ProcessBuilder(
+            nodeModules.resolve(".bin/mocha").toString(),
+            // TODO: escaping
+            srcPath + "/**/*Test.ts",
+            "--require", "ts-node/register",
+            "--ui", "tdd"
+        )
+            .directory(typeScriptRoot.toFile())
+            .redirectErrorStream(true);
+
+        processBuilder.environment().put("NODE_PATH", nodeModules.toString());
+
+        var process = processBuilder
+            .start();
+
+        var output = new BufferedReader(new InputStreamReader(process.getInputStream()))
+            .lines()
+            .map(
+                line -> line.replaceAll("\\([0-9.]+ms\\)", "(TIME)")
+            )
+            .collect(Collectors.joining("\n"));
+
+        process.waitFor();
+        return output;
     }
 }

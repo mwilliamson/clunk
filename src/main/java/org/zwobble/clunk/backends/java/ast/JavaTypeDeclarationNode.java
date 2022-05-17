fedcdfc3b2b116905eae025f151339f51b274b99
Michael Williamson <mike@zwobble.org>
2022-05-17 20:31:26 +0100

    Add name() to JavaTypeDeclarationNode

--- src/main/java/org/zwobble/clunk/backends/java/ast/JavaTypeDeclarationNode.java
+++ src/main/java/org/zwobble/clunk/backends/java/ast/JavaTypeDeclarationNode.java
@@ -1,6 +1,7 @@
 package org.zwobble.clunk.backends.java.ast;
 
 public interface JavaTypeDeclarationNode extends JavaNode {
+    String name();
     <T> T accept(Visitor<T> visitor);
 
     interface Visitor<T> {

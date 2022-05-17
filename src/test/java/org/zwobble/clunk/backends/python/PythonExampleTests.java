3ae177c5b8fdf6d5de3f5499d2a76863069ca327
Michael Williamson <mike@zwobble.org>
2022-05-17 20:23:19 +0100

    Optimise imports

--- src/test/java/org/zwobble/clunk/backends/python/PythonExampleTests.java
+++ src/test/java/org/zwobble/clunk/backends/python/PythonExampleTests.java
@@ -2,23 +2,12 @@ package org.zwobble.clunk.backends.python;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.zwobble.clunk.Compiler;
-import org.zwobble.clunk.backends.TargetTestRunner;
-import org.zwobble.clunk.logging.Logger;
 import org.zwobble.clunk.testing.snapshots.SnapshotResolver;
 import org.zwobble.clunk.testing.snapshots.Snapshotter;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import static org.zwobble.clunk.backends.ExampleTests.runExampleTest;
-import static org.zwobble.clunk.testing.ProjectRoot.findRoot;
-import static org.zwobble.clunk.util.DeleteRecursive.deleteRecursive;
 
 @ExtendWith(SnapshotResolver.class)
 public class PythonExampleTests {

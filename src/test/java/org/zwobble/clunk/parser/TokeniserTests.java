3acd68a79656238986ff019adf2d0feecd9a5107
Michael Williamson <mike@zwobble.org>
2022-07-17 15:37:46 +0100

    Add test for skipping whitespace

---
+++ src/test/java/org/zwobble/clunk/parser/TokeniserTests.java
@@ -0,0 +1,23 @@
+package org.zwobble.clunk.parser;
+
+import org.junit.jupiter.api.Test;
+import org.zwobble.clunk.sources.FileFragmentSource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class TokeniserTests {
+    @Test
+    public void whitespaceTokensAreIgnored() {
+        var source = FileFragmentSource.create("x.clunk", """
+            1 "two" 3
+            """);
+
+        var result = Tokeniser.tokenise(source);
+
+        assertThat(result.nextValue(TokenType.INT), equalTo("1"));
+        assertThat(result.nextValue(TokenType.STRING), equalTo("\"two\""));
+        assertThat(result.nextValue(TokenType.INT), equalTo("3"));
+        assertThat(result.trySkip(TokenType.END), equalTo(true));
+    }
+}

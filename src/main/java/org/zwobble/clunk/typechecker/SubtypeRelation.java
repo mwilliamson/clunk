1ea735544b539facc737b783ecf271dbcb8f6236
Michael Williamson <mike@zwobble.org>
2022-08-02 22:43:34 +0100

    Delete unused class

--- src/main/java/org/zwobble/clunk/typechecker/SubtypeRelation.java
+++
@@ -1,7 +0,0 @@
-package org.zwobble.clunk.typechecker;
-
-import org.zwobble.clunk.types.InterfaceType;
-import org.zwobble.clunk.types.RecordType;
-
-public record SubtypeRelation(RecordType subtype, InterfaceType supertype) {
-}

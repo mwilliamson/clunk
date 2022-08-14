8979e5fef3cf831a2d39c73c92eda6a3b6670315
Michael Williamson <mike@zwobble.org>
2022-08-14 15:05:22 +0100

    Add TypedReferenceNode interface

--- src/main/java/org/zwobble/clunk/ast/typed/TypedLocalReferenceNode.java
+++ src/main/java/org/zwobble/clunk/ast/typed/TypedLocalReferenceNode.java
@@ -3,7 +3,7 @@ package org.zwobble.clunk.ast.typed;
 import org.zwobble.clunk.sources.Source;
 import org.zwobble.clunk.types.Type;
 
-public record TypedLocalReferenceNode(String name, Type type, Source source) implements TypedExpressionNode {
+public record TypedLocalReferenceNode(String name, Type type, Source source) implements TypedReferenceNode {
     @Override
     public <T> T accept(Visitor<T> visitor) {
         return visitor.visit(this);
---
+++ src/main/java/org/zwobble/clunk/ast/typed/TypedReferenceNode.java
@@ -0,0 +1,5 @@
+package org.zwobble.clunk.ast.typed;
+
+public interface TypedReferenceNode extends TypedExpressionNode {
+    String name();
+}

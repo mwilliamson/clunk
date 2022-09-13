aa8553d4ff67a6991c4b992c4ff27b9b0d02e1f9
Michael Williamson <mike@zwobble.org>
2022-09-13 15:26:36 +0100

    Enable string builder example for Python

--- src/test/java/org/zwobble/clunk/backends/python/PythonExampleTests.java
+++ src/test/java/org/zwobble/clunk/backends/python/PythonExampleTests.java
@@ -1,11 +1,15 @@
 package org.zwobble.clunk.backends.python;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.zwobble.clunk.backends.Backend;
 import org.zwobble.clunk.backends.ExampleTests;
 import org.zwobble.clunk.backends.TargetTestRunner;
 import org.zwobble.clunk.logging.Logger;
 import org.zwobble.clunk.testing.snapshots.SnapshotResolver;
+import org.zwobble.clunk.testing.snapshots.Snapshotter;
+
+import java.io.IOException;
 
 @ExtendWith(SnapshotResolver.class)
 public class PythonExampleTests extends ExampleTests {
@@ -17,5 +21,13 @@ public class PythonExampleTests extends ExampleTests {
     @Override
     protected TargetTestRunner targetTestRunner() {
         return new PythonTestRunner();
+    }
+
+    @Test
+    public void stringBuilder(Snapshotter snapshotter) throws IOException, InterruptedException {
+        runExampleTest(
+            snapshotter,
+            "stringBuilder"
+        );
     }
 }

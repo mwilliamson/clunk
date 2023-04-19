ef59a2f2bc8c675452e7c690a8ab5af72a55d572
Michael Williamson <mike@zwobble.org>
2023-04-19 20:20:39 +0100

    Add hasMember and isSome matcher types

--- src/main/java/org/zwobble/clunk/builtins/BuiltinsMatchers.java
+++ src/main/java/org/zwobble/clunk/builtins/BuiltinsMatchers.java
@@ -37,7 +37,51 @@ public class BuiltinsMatchers {
         Visibility.PUBLIC
     );
 
+    private static final TypeParameter HAS_MEMBER_TYPE_PARAM_RECEIVER = TypeParameter.function(
+        NAMESPACE_ID,
+        "hasMember",
+        "T"
+    );
+
+    private static final TypeParameter HAS_MEMBER_TYPE_PARAM_MEMBER = TypeParameter.function(
+        NAMESPACE_ID,
+        "hasMember",
+        "R"
+    );
+
+    static final StaticFunctionType HAS_MEMBER_TYPE = new StaticFunctionType(
+        NAMESPACE_ID,
+        "hasMember",
+        Optional.of(List.of(HAS_MEMBER_TYPE_PARAM_RECEIVER, HAS_MEMBER_TYPE_PARAM_MEMBER)),
+        new ParamTypes(
+            List.of(
+                Types.member(HAS_MEMBER_TYPE_PARAM_RECEIVER, HAS_MEMBER_TYPE_PARAM_MEMBER),
+                matcher(HAS_MEMBER_TYPE_PARAM_MEMBER)
+            ),
+            List.of()
+        ),
+        matcher(HAS_MEMBER_TYPE_PARAM_RECEIVER),
+        Visibility.PUBLIC
+    );
+
+    private static final TypeParameter IS_SOME_TYPE_PARAM = TypeParameter.function(
+        NAMESPACE_ID,
+        "isSome",
+        "T"
+    );
+
+    static final StaticFunctionType IS_SOME_TYPE = new StaticFunctionType(
+        NAMESPACE_ID,
+        "isSome",
+        Optional.of(List.of(IS_SOME_TYPE_PARAM)),
+        new ParamTypes(List.of(matcher(IS_SOME_TYPE_PARAM)), List.of()),
+        matcher(Types.option(IS_SOME_TYPE_PARAM)),
+        Visibility.PUBLIC
+    );
+
     static final NamespaceType NAMESPACE_TYPE = new NamespaceType(NAMESPACE_ID, Map.ofEntries(
-        Map.entry("equalTo", EQUAL_TO_TYPE)
+        Map.entry("equalTo", EQUAL_TO_TYPE),
+        Map.entry("hasMember", HAS_MEMBER_TYPE),
+        Map.entry("isSome", IS_SOME_TYPE)
     ));
 }
